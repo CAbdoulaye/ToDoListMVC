a42e2d134862a2488b60082837f0dc1d83df6e69
CAbdoulaye <c_faye@mail.fhsu.edu>
2024-05-11 21:09:10 -0500

    first commite

--- VIEW/header.php
+++ VIEW/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do List Assignment</title>
-    <link rel="stylesheet" href="./VIEW/CSS/styles.css"> 
+    <link rel="stylesheet" href="./VIEW/CSS/style.css"> 
 </head>
 <body>
   <header>
